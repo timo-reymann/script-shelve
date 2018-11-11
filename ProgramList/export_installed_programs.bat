041fb8d90ccae30ab1e08578a684752a4697c0d5
Timo Reymann <mail@timo-reymann.de>
2018-11-11 14:59:21 +0100

    Add export installed programs script

---
+++ ProgramList/export_installed_programs.bat
@@ -0,0 +1,5 @@
+REM 
+REM This script will generate a csv file located at C:/programs.txt containing version and name for all installed programs
+REM
+
+wmic product get name,version /format:csv > C:/programs.txt
