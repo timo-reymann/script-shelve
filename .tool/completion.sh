e5df583ac76af9044fc2a43270fb5da6fc24b536
Timo Reymann <Timo.Reymann@trustedshops.de>
2021-09-08 14:14:23 +0200

    fix: Fix autocomplete to not only include sh files

--- .tool/completion.sh
+++ .tool/completion.sh
@@ -3,7 +3,8 @@
 _script-shelve_completion() {
     dir=$(dirname $(readlink $(which script-shelve)))
     dir=$(cd $dir && pwd)
-    COMPREPLY=($(cd $dir && cd .. && compgen -W "$(find . -type f  -not -path '*/\.*' -name "*.sh" | sed 's/.\///')" -- "${COMP_WORDS[1]}"))
+
+    COMPREPLY=($(cd $dir && cd .. && compgen -W "$(find . -type f  -not -path '*/\.*' | sed 's/.\///')" -- "${COMP_WORDS[1]}"))
 }
 
 complete -F _script-shelve_completion script-shelve
